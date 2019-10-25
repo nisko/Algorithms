b6d6cf295d10534cd0ff4dfec5aa7285e6f6a795
nisko <nicholai.skobelev@yandex.ru>
2019-10-25 07:40:40 +0300

    added rb_insert

--- python/BinaryTree/RBTree.py
+++ python/BinaryTree/RBTree.py
@@ -50,3 +50,55 @@ class RBTree(BinaryTree.BinaryTree):
 
         left_node.right = pivot_node
         pivot_node.parent = left_node
+
+    def rb_insert_fixup(self, new_node: RBNode):
+        while new_node.parent.colot == "Red":
+            if new_node.parent is new_node.parent.parent.left:
+                fix_node = new_node.parent.parent.right
+                if fix_node.color == "Red":
+                    new_node.parent.color = "Black"
+                    fix_node.color = "Black"
+                    new_node.parent.parent.color = "Red"
+                    new_node = new_node.parent.parent
+                elif new_node is new_node.parent.right:
+                    new_node = new_node.parent
+                    self.left_rotate(new_node)
+
+                new_node.parent.color = "Black"
+                new_node.parent.parent.color = "Red"
+                self.right_rotate(new_node.parent.parent)
+            else:
+                fix_node = new_node.parent.parent.right
+                if fix_node.color == "Red":
+                    new_node.parent.color = "Black"
+                    fix_node.color = "Black"
+                    new_node.parent.parent.color = "Red"
+                    new_node = new_node.parent.parent
+                elif new_node is new_node.parent.right:
+                    new_node = new_node.parent
+                    self.right_rotate(new_node)
+
+                new_node.parent.color = "Black"
+                new_node.parent.parent.color = "Red"
+                self.left_rotate(new_node.parent.parent)
+
+    def rb_insert(self, new_node: RBNode):
+        node_y = None
+        node_x = self.root
+        while node_x is not None:
+            node_y = node_x
+            if new_node.key < node_x.key:
+                node_x = node_x.left
+            else:
+                node_x = node_x.right
+        new_node.parent = node_y
+        if node_y is None:
+            self.root = new_node
+        elif new_node.key < node_y.key:
+            node_y.left = new_node
+        else:
+            node_y.right = new_node
+        new_node.left = None
+        new_node.right = None
+        new_node.color = "Red"
+        self.rb_insert_fixup(new_node)
